5d1cb8ded088dfe3694ed444f8fb32820ff30c3a
archiblue22 <paguirre@byui.edu>
2020-09-27 14:16:33 -0600

    final commit

--- js/lastmodified.js
+++ js/lastmodified.js
@@ -1,1 +1,6 @@
 document.getElementById("lastmodified").innerHTML = document.lastModified
+
+let d = new Date();
+let year = d.getFullYear();
+let currentyear = year;
+document.getElementById("getFullYear").textContent = currentyear;
