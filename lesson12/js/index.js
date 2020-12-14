d67ea74a322ae23bee4cfaaf11e8527a6dd5be6a
archiblue22 <paguirre@byui.edu>
2020-12-14 12:07:05 -0700

    commit

--- lesson12/js/index.js
+++ lesson12/js/index.js
@@ -1,16 +1,6 @@
-try {
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-  } catch (e) {
-    alert('Error with code or your browser does not support Locale');
-}
+document.getElementById("lastmodified").innerHTML = document.lastModified
 
-var d = new Date();
-var x = d.getDay();
-
-if (x == 5) {
-    document.getElementById('message').style.display = "block";
-}
-else {
-   document.getElementById("message").style.display = "none";
-}
+let d = new Date();
+let year = d.getFullYear();
+let currentyear = year;
+document.getElementById("getFullYear").textContent = currentyear;
--- lesson12/js/rental.js
+++ lesson12/js/rental.js
@@ -1,3 +1,36 @@
 $.getJSON('data.json', function(data) {
-    //do stuff with your data here
-});
+    function showData(jsonObj) {
+        var data = jsonObj['serviceRentals'];
+    
+        for (var i = 0; i < data.length; i++) {
+            var myRentals = document.createElement('rentals');
+            var myType = document.createElement('type');
+            var myPersons = document.createElement('persons');
+            var myReservationhalf = document.createElement('reservationhalf');
+            var myReservationfull = document.createElement('reservationfull');
+            var myWalkinhalf = document.createElement('walkinhalf');
+            var myWalkinfull = document.createElement('walkinfull');
+    
+            myPersons.textContent = data[i].name;
+            myReservationhalf.textContent = data[i].type;
+            myReservationfull.textContent = data[i].type;
+            myWalkinhalf.textContent = data[i].type;
+            myWalkinfull.textContent = data[i].type;
+    
+            var rental = data[i].items;
+            for (var j = 0; j < rental.length; j++) {
+                var listItem = document.createElement('li');
+                listItem.textContent = rental[j];
+                myList.appendChild(listItem);
+            }
+    
+            myType.appendChild(myPersons);
+            myType.appendChild(myReservationhalf);
+            myType.appendChild(myReservationfull);
+            myType.appendChild(myWalkinhalf);
+            myType.appendChild(myWalkinfull);
+    
+            myRentals.appendChild(myType);
+            aside.appendChild(myRentals);
+        }
+    }});
